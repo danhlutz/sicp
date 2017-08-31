3efa31a50462aeab3b3beafcb8346cfe893ad86a
danhlutz <danhlutz@gmail.com>
2017-08-31 08:11:48 -0400

    Rewrite sqrt function to use lexical scoping

--- chap01/chap01.scm
+++ chap01/chap01.scm
@@ -7,25 +7,10 @@
   (or (> x y) (= x y)))
 
 (define (square x)
-  (* x x))
-
-(define (sqrt-iter guess x)
-  (if (good-enough2? guess x)
-      guess
-      (sqrt-iter (improve guess x)
-                 x)))
-
-(define (improve guess x)
-  (average guess (/ x guess)))
+  (* x x ))
 
 (define (average x y)
   (/ (+ x y) 2))
-
-(define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001))
-
-(define (sqrt x)
-  (sqrt-iter 1.0 x))
 
 (define (new-if predicate then-clause else-clause)
   (cond (predicate then-clause)
@@ -33,3 +18,14 @@
 
 (define (good-enough2? guess x)
   (< (abs (- (improve guess x) guess)) 0.001))
+
+(define (sqrt x)
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess) 
+        guess
+        (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
