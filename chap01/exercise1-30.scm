12eb90de7097b084645494e10b91277e11451b0e
danhlutz <danhlutz@gmail.com>
2017-09-08 22:12:47 -0400

    Reimplement sum as an iterative procedure

---
+++ chap01/exercise1-30.scm
@@ -0,0 +1,10 @@
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ (term a) result))))
+  (iter a 0))
+
+(define (cube x) (* x x x))
+
+(define (inc n) (+ n 1))
