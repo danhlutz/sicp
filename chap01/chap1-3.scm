5da66270cd835b54f5960e9a92ab7b8c084687aa
danhlutz <danhlutz@gmail.com>
2017-09-10 21:34:27 -0400

    Rewrite integral using a lambda

--- chap01/chap1-3.scm
+++ chap01/chap1-3.scm
@@ -8,11 +8,10 @@
   (sum cube a inc b))
 
 (define (pi-sum a b)
-  (define (pi-term x)
-    (/ 1.0 (* x (+ x 2))))
-  (define (pi-next x)
-    (+ x 4))
-  (sum pi-term a pi-next b))
+  (sum (lambda (x) (/ 1.0 (* x (+ x 2))))
+       a 
+       (lambda (x) (+ x 4))
+       b))
 
 (define (sum term a next b)
   (if (> a b)
@@ -26,6 +25,15 @@
 (define (identity x) x)
 
 (define (integral f a b dx)
-  (define (add-dx x) (+ x dx))
-  (* (sum f (+ a (/ dx 2.0)) add-dx b)
+  (* (sum f 
+          (+ a (/ dx 2.0)) 
+          (lambda (x) (+ x dx)) 
+          b)
      dx))
+
+(define (f x y)
+  (let ((a (+ 1 (* x y)))
+        (b (- 1 y)))
+    (+ (* x (square a))
+       (* y b)
+       (* a b))))
