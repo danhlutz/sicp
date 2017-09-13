e99b32d47e1eecec9601d4950aecb092dfe48b33
danhlutz <danhlutz@gmail.com>
2017-09-13 08:26:00 -0400

    Add derivative and newtons method

--- chap01/chap1-3.scm
+++ chap01/chap1-3.scm
@@ -78,5 +78,30 @@
   (try first-guess))
 
 (define (sqrt x)
-  (fixed-point (lambda (y) (average y (/ x y)))
+  (fixed-point (average-damp (lambda (y) (/ x y)))
                1.0))
+
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+
+(define (cube-root x)
+  (fixed-point (average-damp (lambda (y) (/ x (square y))))
+               1.0))
+
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(define dx 0.00001)
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+(define (sqrt2 x)
+  (newtons-method (lambda (y) (- (square y) x))
+                  1.0))
