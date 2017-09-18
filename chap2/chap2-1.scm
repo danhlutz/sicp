45cb6101ffbd9ac029b29063401c116208c7dd4b
danhlutz <danhlutz@gmail.com>
2017-09-18 17:53:48 -0400

    Define and print rational nums

--- chap2/chap2-1.scm
+++ chap2/chap2-1.scm
@@ -19,3 +19,17 @@
 (define (equal-rat? x y)
   (= (* (numer x) (denom y))
      (* (denom x) (numer y))))
+
+(define (make-rat n d) 
+  (let ((g (gcd n d)))
+    (cons (/ n g) (/ d g))))
+
+(define (numer x) (car x))
+
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
