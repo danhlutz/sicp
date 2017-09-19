9d2d80967bad7d83b4a3816193e0979e9ca51d05
danhlutz <danhlutz@gmail.com>
2017-09-19 07:47:57 -0400

    Construct points and segments, and compute midpoint of a segment

---
+++ chap2/ex02.scm
@@ -0,0 +1,34 @@
+(define (make-segment start end)
+  (cons start end))
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cdr segment))
+
+(define (make-point x-point y-point)
+  (cons x-point y-point))
+
+(define (x-point point)
+  (car point))
+
+(define (y-point point)
+  (cdr point))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(define (average a b)
+  (/ (+ a b) 2.0))
+
+(define (midpoint segment)
+  (make-point (average (x-point (start-segment segment))
+                       (x-point (end-segment segment)))
+              (average (y-point (start-segment segment))
+                       (y-point (end-segment segment)))))
