ad8791a390cc3f907231fbb50b359cb657941916
danhlutz <danhlutz@gmail.com>
2017-09-25 08:21:35 -0400

    Measure and append lists

---
+++ chap2/chap2-2.scm
@@ -0,0 +1,18 @@
+(define nil (list))
+
+(define (list-ref items n)
+  (if (= n 0)
+      (car items)
+      (list-ref (cdr items) (- n 1))))
+
+(define (length items)
+  (define (length-iter a count)
+    (if (null? a)
+        count
+        (length-iter (cdr a) (+ count 1))))
+  (length-iter items 0))
+
+(define (append list1 list2)
+  (if (null? list1)
+      list2
+      (cons (car list1) (append (cdr list1) list2))))
