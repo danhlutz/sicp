cf192f6edd5b5dbbe240499a92b1f3881b45a553
danhlutz <danhlutz@gmail.com>
2017-09-28 07:54:50 -0400

    Add map

--- chap2/chap2-2.scm
+++ chap2/chap2-2.scm
@@ -16,3 +16,20 @@
   (if (null? list1)
       list2
       (cons (car list1) (append (cdr list1) list2))))
+
+; replaced below
+; (define (scale-list items factor)
+;   (if (null? items)
+;       nil
+;      (cons (* (car items) factor)
+;            (scale-list (cdr items) factor))))
+
+(define (map proc items)
+  (if (null? items)
+      nil
+      (cons (proc (car items))
+            (map proc (cdr items)))))
+
+(define (scale-list items factor)
+  (map (lambda (x) (* x factor))
+       items))
