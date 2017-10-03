75186d59462d11cb935a6e3a339003d6e9df6ee9
danhlutz <danhlutz@gmail.com>
2017-10-03 08:17:06 -0400

    Map, filter, and accumulate sequences

--- chap2/chap2-2-3.scm
+++ chap2/chap2-2-3.scm
@@ -17,12 +17,12 @@
                         q
                         (- count 1)))))
 
-(define (sum-odd-squares tree)
-  (cond ((null? tree) 0)
-        ((not (pair? tree))
-         (if (odd? tree) (square tree) 0))
-        (else (+ (sum-odd-squares (car tree))
-                 (sum-odd-squares (cdr tree))))))
+;(define (sum-odd-squares tree)
+;  (cond ((null? tree) 0)
+;        ((not (pair? tree))
+;         (if (odd? tree) (square tree) 0))
+;        (else (+ (sum-odd-squares (car tree))
+;                 (sum-odd-squares (cdr tree))))))
 
 (define a (list 1
                 (list 2 (list 3 4) 5)
@@ -30,12 +30,62 @@
                 8
                 (list 9 (list 10 11 12) 13)))
 
+;(define (even-fibs n)
+;  (define (next k)
+;    (if (> k n)
+;        nil
+;        (let ((f (fib k)))
+;          (if (even? f)
+;              (cons f (next (+ k 1)))
+;              (next (+ k 1))))))
+;  (next 0))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
+
+(define (sum-odd-squares tree)
+  (accumulate +
+              0
+              (map square
+                   (filter odd? (enumerate-tree tree)))))
+
 (define (even-fibs n)
-  (define (next k)
-    (if (> k n)
-        nil
-        (let ((f (fib k)))
-          (if (even? f)
-              (cons f (next (+ k 1)))
-              (next (+ k 1))))))
-  (next 0))
+  (accumulate cons
+              nil
+              (filter even?
+                      (map fib
+                           (enumerate-interval 0 n)))))
+
+(define (list-fib-squares n)
+  (accumulate cons
+              nil
+              (map square
+                   (map fib
+                        (enumerate-interval 0 n)))))
+
+(define (product-of-squares-of-odd-elements sequence)
+  (accumulate *
+              1
+              (map square
+                   (filter odd? sequence))))
