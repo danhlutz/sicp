e07a1a219c49e2c91dbaa285814a7f2f4b847d2d
danhlutz <danhlutz@gmail.com>
2017-10-02 21:26:46 -0400

    Look at two functions before decomposing them into signal processing functions

---
+++ chap2/chap2-2-3.scm
@@ -0,0 +1,41 @@
+(define nil (list))
+
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+        ((even? count)
+         (fib-iter a
+                   b
+                   (+ (* p p) (* q q))
+                   (+ (* 2 p q) (* q q))
+                   (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)))))
+
+(define (sum-odd-squares tree)
+  (cond ((null? tree) 0)
+        ((not (pair? tree))
+         (if (odd? tree) (square tree) 0))
+        (else (+ (sum-odd-squares (car tree))
+                 (sum-odd-squares (cdr tree))))))
+
+(define a (list 1
+                (list 2 (list 3 4) 5)
+                (list 6 7)
+                8
+                (list 9 (list 10 11 12) 13)))
+
+(define (even-fibs n)
+  (define (next k)
+    (if (> k n)
+        nil
+        (let ((f (fib k)))
+          (if (even? f)
+              (cons f (next (+ k 1)))
+              (next (+ k 1))))))
+  (next 0))
