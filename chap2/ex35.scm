6694670db8395c0fb462bd1f8ec49f2f7f3fb9da
danhlutz <danhlutz@gmail.com>
2017-10-04 08:20:56 -0400

    Count leaves using accumulate and map

---
+++ chap2/ex35.scm
@@ -0,0 +1,19 @@
+(define nil (list))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
+
+(define (count-leaves tree)
+  (accumulate +
+              0
+              (map (lambda (x) 1)
+                   (enumerate-tree tree))))
