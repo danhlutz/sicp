b54fd87fa5c0bfd328c558ceb081bd1f819e8559
danhlutz <danhlutz@gmail.com>
2017-10-05 19:57:22 -0400

    Use flatmap for nested loops

--- chap2/chap2-2-3.scm
+++ chap2/chap2-2-3.scm
@@ -89,3 +89,62 @@
               1
               (map square
                    (filter odd? sequence))))
+
+; prime? 
+
+(define (next n)
+  (if (= n 2)
+      3
+      (+ n 2)))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (next test-divisor )))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= (smallest-divisor n) n))
+
+; flatmpaping 
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (prime-pair? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-pair?
+               (flatmap
+                 (lambda (i)
+                   (map (lambda (j) (list i j))
+                        (enumerate-interval 1 (- i 1))))
+                 (enumerate-interval 1 n)))))
+
+(define (permutations s)
+  (newline)
+  (display "set: ")
+  (display s)
+  (if (null? s)
+      (list nil)
+      (flatmap (lambda (x)
+                 (newline)
+                 (display "removing x: ")
+                 (display x)
+                 (map (lambda (p) (cons x p))
+                      (permutations (remove x s))))
+               s)))
+
+(define (remove item sequence)
+  (filter (lambda (x) (not (= item x)))
+          sequence))
