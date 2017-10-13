31c52ec684898e3b96e25497213461855ae8344a
danhlutz <danhlutz@gmail.com>
2017-10-13 08:20:38 -0400

    Use a new split procedure to rewrite up-split and right-split

---
+++ chap2/ex45.rkt
@@ -0,0 +1,42 @@
+#lang racket
+
+(require (planet "sicp.ss" ("soegaard" "sicp.plt" 2 1)))
+
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left (beside up up))
+              (bottom-right (below right right))
+              (corner (corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
+
+(define (square-limit painter n)
+  (let ((quarter (corner-split painter n)))
+    (let ((half (beside (flip-horiz quarter) quarter)))
+      (below (flip-vert half) half))))
+
+; (define (right-split painter n)
+;  (if (= n 0)
+;      painter
+;      (let ((smaller (right-split painter (- n 1))))
+;        (beside painter (below smaller smaller)))))
+
+; (define (up-split painter n)
+;  (if (= n 0)
+;      painter
+;      (let ((smaller (up-split painter (- n 1))))
+;        (below painter (beside smaller smaller)))))
+
+(define (split outer inner)
+  (lambda (painter n)
+    (if (= n 0)
+        painter
+        (let ((smaller ((split outer inner) painter (- n 1))))
+          (outer painter (inner smaller smaller))))))
+
+(define right-split (split beside below))
+(define up-split (split below beside))
+
