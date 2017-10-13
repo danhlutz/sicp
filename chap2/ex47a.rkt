d29d17dd0734043ffca9965e3758e3f429590ec6
danhlutz <danhlutz@gmail.com>
2017-10-13 19:02:50 -0400

    Create selectors for frame implemented as a list

---
+++ chap2/ex47a.rkt
@@ -0,0 +1,33 @@
+(define (make-vect x y) (cons x y))
+
+(define (xcor-vect v) (car v))
+
+(define (ycor-vect v) (cdr v))
+
+(define (add-vect a b)
+  (make-vect (+ (xcor-vect a)
+                (xcor-vect b))
+             (+ (ycor-vect a)
+                (ycor-vect b))))
+
+(define (sub-vect a b)
+  (make-vect (- (xcor-vect a)
+                (xcor-vect b))
+             (- (ycor-vect a)
+                (ycor-vect b))))
+
+(define (scale-vect s v)
+  (make-vect (* s (xcor-vect v))
+             (* s (ycor-vect v))))
+
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define (origin-frame frame)
+  (car frame))
+
+(define (edge1-frame frame)
+  (car (cdr frame)))
+
+(define (edge2-frame frame)
+  (car (cdr (cdr frame))))
