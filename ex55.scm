d20115cb75670b9abefab89127b664c548b770db
danhlutz <danhlutz@gmail.com>
2017-10-15 19:52:08 -0400

    Explain Eva's weird output

---
+++ ex55.scm
@@ -0,0 +1,6 @@
+; (car ''abracadabra)
+; quote
+
+; Eva called quote on the next object following it - ', the abbreviation
+; for the quote operator
+; so the quote operator returned the quote operator
