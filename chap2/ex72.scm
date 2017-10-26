9ffc8a6b7bb95f37e138ee6d115558bac918f048
danhlutz <danhlutz@gmail.com>
2017-10-26 18:05:47 -0400

    Discuss the orders of growth of encoding a Huffman tree

---
+++ chap2/ex72.scm
@@ -0,0 +1,92 @@
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+
+(define (symbol-leaf x) (cadr x))
+
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+(define (left-branch tree) (car tree))
+
+(define (right-branch tree) (cadr tree))
+
+(define (symbols tree)
+  (if (leaf? tree) 
+      (list (symbol-leaf tree))
+      (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch 
+               (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "bad bit -- CHOOSE BRANCH " bit))))
+
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                    (make-leaf 'B 2)
+                    (make-code-tree (make-leaf 'D 1)
+                                    (make-leaf 'C 1)))))
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((eq? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append (encode-symbol (car message) tree)
+              (encode (cdr message) tree))))
+
+(define (encode-symbol symbol tree)
+  (cond ((leaf? tree) '())
+        ((element-of-set? symbol (symbols (left-branch tree)))
+         (cons 0 (encode-symbol symbol (left-branch tree))))
+        ((element-of-set? symbol (symbols (right-branch tree)))
+         (cons 1 (encode-symbol symbol (right-branch tree))))
+        (else (error "SYMBOL NOT IN TREE -- " symbol))))
+
+; When encoding a sybmol, at each step encoder must check
+; all of the symbols in the left branch and the right branch of the tree.
+; The search method element-of-set? is itself O(n), since
+; in the worst case it must search the entire set to check if an element
+; is a member.
+; If you are encoding the most frequent item, you will find in the first 
+; scan, so that only required O(n)
+; but to encode the least frequent item you must scan the entire tree
+; until you reach the bottom. Since some trees produce relatively
+; balanced trees, the number of steps to get to the bottom 
+; is close to (ln n), giving an overall order of growth as O(n * ln n)
+
+; The example in ex 71 gives a worst case scenario for encoding the
+; least frequent character. Since the tree is unbalanced all the way down
+; it requires n - 1 scans to reach the least frequent character, giving
+; an efficiency of O( n ** 2). 
+; You will encode the most frequent character after only 1 step, giving
+; an order of growth of O(n)
+
