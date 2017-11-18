7c0672aa19d78e924e6b641d37fb3accb8a41649
danhlutz <danhlutz@gmail.com>
2017-11-17 20:57:34 -0500

    Combine dense and sparse representations of polynomials

---
+++ chap2/dense.scm
@@ -0,0 +1,9 @@
+(define d1 (make-dense-polynomial
+             'x
+             (attach-tag 'dense
+                         (list 2 3 0 0 0))))
+
+(define d2 (make-dense-polynomial
+             'x
+             (attach-tag 'dense
+                         (list 1 2))))
