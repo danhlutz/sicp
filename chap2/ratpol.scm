816f3f9eb32acc1d3832d99d687f5712e80e43d0
danhlutz <danhlutz@gmail.com>
2017-11-20 17:11:33 -0500

    The gcd algorithm does not return a larger common multiple of the two polys

--- chap2/ratpol.scm
+++ chap2/ratpol.scm
@@ -1,20 +1,39 @@
-(define p1 (make-polynomial 'x
+(define old-p1 (make-polynomial 'x
                             (attach-tag
                               'sparse
                               (list '(2 1) '(0 1)))))
 
-(define p2 (make-polynomial 'x 
+(define old-p2 (make-polynomial 'x 
                             (attach-tag
                               'sparse 
                               (list '(3 1) '(0 1)))))
 (define rf (make-rational p2 p1))
 
-(define p3 (make-polynomial 'x
+(define old-p3 (make-polynomial 'x
                             (attach-tag
                               'sparse
                               (list '(4 1) '(3 -1) '(2 -2) '(1 2)))))
 
-(define p4 (make-polynomial 'x
+(define old-p4 (make-polynomial 'x
                             (attach-tag
                               'sparse
                               (list '(3 1) '(1 -1)))))
+
+(define p1 (make-polynomial 'x
+                            (attach-tag
+                              'sparse
+                              (list '(2 1) '(1 -2) '(0 1)))))
+
+(define p2 (make-polynomial 'x
+                            (attach-tag
+                              'sparse
+                              (list '(2 11) '(0 7)))))
+
+(define p3 (make-polynomial 'x
+                            (attach-tag 
+                              'sparse
+                              (list '(1 13) '(0 5)))))
+
+(define Q1 (mul p1 p2))
+
+(define Q2 (mul p1 p3))
