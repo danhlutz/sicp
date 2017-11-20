aa49a30b72331c612adf7d8c1e8d6a016ab1cabd
danhlutz <danhlutz@gmail.com>
2017-11-20 16:59:37 -0500

    Find the gcd of two polynomials

--- chap2/ratpol.scm
+++ chap2/ratpol.scm
@@ -8,3 +8,13 @@
                               'sparse 
                               (list '(3 1) '(0 1)))))
 (define rf (make-rational p2 p1))
+
+(define p3 (make-polynomial 'x
+                            (attach-tag
+                              'sparse
+                              (list '(4 1) '(3 -1) '(2 -2) '(1 2)))))
+
+(define p4 (make-polynomial 'x
+                            (attach-tag
+                              'sparse
+                              (list '(3 1) '(1 -1)))))
