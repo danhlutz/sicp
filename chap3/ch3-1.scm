68251dbc52f7b43e050636eef8805d5504dfdbe4
danhlutz <danhlutz@gmail.com>
2017-11-25 14:26:05 -0500

    Assign balance as a local variable to new-withdraw

--- chap3/ch3-1.scm
+++ chap3/ch3-1.scm
@@ -5,3 +5,12 @@
       (begin (set! balance (- balance amount))
              balance)
       "Insufficient funds"))
+
+(define new-withdraw
+  (let ((balance 100))
+    (lambda (amount)
+      (if (>= balance amount)
+          (begin (set! balance (- balance amount))
+                 balance)
+          "Insufficient funds"))))
+
