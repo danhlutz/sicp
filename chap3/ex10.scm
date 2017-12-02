05aa2d227651625abd731f99b831f815b9fe2c67
danhlutz <danhlutz@gmail.com>
2017-12-01 20:11:22 -0500

    Model the behavior of using let with state

---
+++ chap3/ex10.scm
@@ -0,0 +1,8 @@
+(define (make-withdraw initial-amount)
+  ((lambda (balance)
+    (lambda (amount)
+      (if (>= balance amount)
+          (begin (set! balance (- balance amount))
+                 balance)
+          "Insufficient funds")))
+  initial-amount))
