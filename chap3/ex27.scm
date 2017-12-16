a0c91a567dd233d1c14db54e008eb8b15b117d6d
danhlutz <danhlutz@gmail.com>
2017-12-16 12:09:16 -0500

    Memoize fib

---
+++ chap3/ex27.scm
@@ -0,0 +1,25 @@
+(load "ex25.scm")
+
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+
+(define (memoize f)
+  (let ((table (make-table)))
+    (lambda (x)
+      (let ((previously-computed-result (lookup (list x) table)))
+        (or previously-computed-result
+            (let ((result (f x)))
+              (insert! (list x) result table)
+              result))))))
+
+(define memo-fib
+  (memoize (lambda (n)
+             (cond ((= n 0) 0)
+                   ((= n 1) 1)
+                   (else (+ (memo-fib (- n 1))
+                            (memo-fib (- n 2))))))))
+
+(define memo-fib2 (memoize fib))
