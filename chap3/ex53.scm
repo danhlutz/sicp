a35a17e60dac801c3e5558a868805d5416f53eda
danhlutz <danhlutz@gmail.com>
2018-01-18 08:15:04 -0500

    Define a stream by adding it to itself

---
+++ chap3/ex53.scm
@@ -0,0 +1,20 @@
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc . argstreams)
+  (if (null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+        (apply proc (map stream-car argstreams))
+        (apply stream-map
+               (cons proc (map stream-cdr argstreams))))))
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+(define s (cons-stream 1 (add-streams s s)))
+
+; this stream doubles itself at each step
+; (1 2 4 8 16 32 ...)
