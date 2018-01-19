e95f5f3340e53e1f4a718070bf9a509cb2daec91
danhlutz <danhlutz@gmail.com>
2018-01-18 20:31:58 -0500

    Define a partial sum as a stream

---
+++ chap3/ex55.scm
@@ -0,0 +1,30 @@
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc . argstreams)
+  (if (null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+        (apply proc (map stream-car argstreams))
+        (apply stream-map
+               (cons proc (map stream-cdr argstreams))))))
+
+(define (integers-starting-from n)
+  (cons-stream n (integers-starting-from (+ n 1))))
+
+(define integers (integers-starting-from 1))
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+
+(define factorials (cons-stream 1 (mul-streams integers factorials)))
+
+(define (partial-sum s)
+  (define (iter s)
+    (cons-stream 0 (add-streams s (iter s))))
+  (stream-cdr (iter s)))
+
+
