935a0897b3440063709247fa9467c5411662cc54
danhlutz <danhlutz@gmail.com>
2018-01-22 08:10:20 -0500

    Expand a rational number into a decimal

---
+++ chap3/ex58.scm
@@ -0,0 +1,54 @@
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc . argstreams)
+  (if (null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+        (apply proc (map stream-car argstreams))
+        (apply stream-map
+               (cons proc (map stream-cdr argstreams))))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (display-stream s times)
+  (define (iter s times-done)
+    (if (> times-done times)
+        'done
+        (begin (display-line (stream-car s))
+               (iter (stream-cdr s) (+ times-done 1)))))
+  (iter s 0))
+
+(define (display-line x)
+  (newline)
+  (display x))
+
+(define (integers-starting-from n)
+  (cons-stream n (integers-starting-from (+ n 1))))
+
+(define integers (integers-starting-from 1))
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+(define (scale-stream s factor)
+  (stream-map (lambda (x) (* x factor)) s))
+
+(define (expand num den radix)
+  (cons-stream
+    (quotient (* num radix) den)
+    (expand (remainder (* num radix) den) den radix)))
+
+(define one-by-seven (expand 1 7 10))
+
+(define three-by-eight (expand 3 8 10))
+
+; expand carries out the long division of the numerator by the denominator
+; in the base of radix. 
+; in order to work, the numerator must be smaller than the denominator
