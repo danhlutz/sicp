ecbf2705b99cf0080be632069dbf0c662fa98b4f
danhlutz <danhlutz@gmail.com>
2018-01-23 17:44:38 -0500

    Keep computing a stream until difference is below the tolerance

---
+++ chap3/ex64.scm
@@ -0,0 +1,75 @@
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc . argstreams)
+  (if (null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+        (apply proc (map stream-car argstreams))
+        (apply stream-map
+               (cons proc (map stream-cdr argstreams))))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (display-stream s times)
+  (define (iter s times-done)
+    (if (> times-done times)
+        'done
+        (begin (display-line (stream-car s))
+               (iter (stream-cdr s) (+ times-done 1)))))
+  (iter s 0))
+
+(define (display-line x)
+  (newline)
+  (display x))
+
+(define (integers-starting-from n)
+  (cons-stream n (integers-starting-from (+ n 1))))
+
+(define integers (integers-starting-from 1))
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+(define (scale-stream s factor)
+  (stream-map (lambda (x) (* x factor)) s))
+
+(define (divide-elements s1 s2)
+  (stream-map / s1 s2))
+
+(define (partial-sums s)
+  (define (iter s)
+    (cons-stream 0 (add-streams s (iter s))))
+  (stream-cdr (iter s)))
+
+; SQRT APPROXIMATION AS A STREAM OF GUESSES
+
+(define (average a b) (/ (+ a b) 2))
+
+(define (sqrt-improve guess x)
+  (average guess (/ x guess)))
+
+(define (sqrt-stream x)
+  (define guesses
+    (cons-stream 1.0
+                 (stream-map (lambda (guess)
+                               (sqrt-improve guess x))
+                             guesses)))
+  guesses)
+
+(define (stream-limit s tolerance)
+  (cond ((stream-null? s) 'done)
+        ((< (abs (- (stream-car s)
+                    (stream-car (stream-cdr s))))
+            tolerance)
+         (stream-car (stream-cdr s)))
+        (else (stream-limit (stream-cdr s) tolerance))))
+
+(define (sqrt-tol x tolerance)
+  (stream-limit (sqrt-stream x) tolerance))
