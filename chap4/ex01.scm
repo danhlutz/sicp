ab8aa76fd332ac37d48b802b6d7132366d08dbe2
danhlutz <danhlutz@gmail.com>
2018-02-05 07:52:37 -0500

    Evaluate expressions right to left

--- chap4/ex01.scm
+++ chap4/ex01.scm
@@ -4,12 +4,19 @@
       (cons (eval (first-operand exps) env)
             (list-of-values (rest-operands exps) env))))
 
-(define (list-L->R exps env)
+(define (list-done-L-R exps env)
   (if (no-operands? exps)
       '()
-      (begin
-        (let ((new-value (eval (first-operand exps) env)))
-          (cons new-value (list-L->R (rest-operands exps) env))))))
+      (let ((left (eval (first-operand exps) env)))
+        (cons left (list-done-L-R (rest-operands exps) env)))))
+
+(define (list-done-R-L exps env)
+  (if (no-operands? exps)
+      '()
+      (let ((right (list-done-R-L (rest-operands exps) env)))
+        (cons (eval (first-operand exps) env)
+              right))))
+
 
 (define (no-operands? exps) (null? exps))
 
