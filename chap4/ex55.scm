8a597d97c1c6821f360dccbad13dd9001f7f2770
danhlutz <danhlutz@gmail.com>
2018-03-22 17:55:09 -0400

    Write sample logic queries

---
+++ chap4/ex55.scm
@@ -0,0 +1,10 @@
+;; Sample queries
+
+;; everyone supervised by Ben 
+(supervisor ?x (Bitdiddle Ben))
+
+;; everyone in accounting
+(job ?x (accounting . ?specialty))
+
+;; everyone in Slumerville
+(address ?x (Slumerville . ?street))
