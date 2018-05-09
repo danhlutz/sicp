de818bbb0626d8a2623a2949a8b73a7a20efc5de
danhlutz <danhlutz@gmail.com>
2018-05-09 08:19:38 -0400

    Add COND to evaluator

--- chap5/ec-eval.scm
+++ chap5/ec-eval.scm
@@ -196,6 +196,45 @@
 (define the-global-environment (setup-environment))
 (define (get-global-environment) the-global-environment)
 
+;; DERIVED EXPRESSIONS
+
+(define (cond? exp) (tagged-list? exp 'cond))
+
+(define (cond-clauses exp) (cdr exp))
+
+(define (cond-else-clause? clause)
+  (eq? (cond-predicate clause) 'else))
+
+(define (cond-predicate clause) (car clause))
+
+(define (cond-actions clause) (cdr clause))
+
+(define (cond->if exp)
+  (expand-clauses (cond-clauses exp)))
+
+(define (expand-clauses clauses)
+  (if (null? clauses)
+      'false
+      (let ((first (car clauses))
+            (rest (cdr clauses)))
+        (if (cond-else-clause? first)
+            (if (null? rest)
+                (sequence->exp (cond-actions first))
+                (error "ELSE clause isn't last -- COND->IF"
+                       clauses))
+            (make-if (cond-predicate first)
+                     (sequence->exp (cond-actions first))
+                     (expand-clauses rest))))))
+
+(define (sequence->exp seq)
+  (cond ((null? seq) seq)
+        ((last-exp? seq) (first-exp seq))
+        (else (make-begin seq))))
+
+(define (make-begin seq) (cons 'begin seq))
+
+(define (make-if predicate consequent alternative)
+  (list 'if predicate consequent alternative))
 
 ;; ERROR MESSAGES
 (define unknown-expression-type-error
@@ -285,6 +324,8 @@
         (list 'user-print user-print)
         (list 'prompt-for-input prompt-for-input)
         (list 'exit? exit?)
+        (list 'cond? cond?)
+        (list 'cond->if cond->if)
           ))
 
 (define eceval
@@ -334,6 +375,8 @@
         (branch (label ev-begin))
         (test (op exit?) (reg exp))
         (branch (label ev-done))
+        (test (op cond?) (reg exp))
+        (branch (label ev-cond))
         (test (op application?) (reg exp))
         (branch (label ev-application))
         (goto (label unknown-expression-type))
@@ -449,6 +492,9 @@
         (goto (label eval-dispatch))
       ev-if-consequent
         (assign exp (op if-consequent) (reg exp))
+        (goto (label eval-dispatch))
+      ev-cond
+        (assign exp (op cond->if) (reg exp))
         (goto (label eval-dispatch))
       ev-assignment
         (assign unev (op assignment-variable) (reg exp))
